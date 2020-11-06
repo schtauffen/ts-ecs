f61e7fb21476eca19523f0ed586459aa616b3ea9
Zach Dahl <z.schtauffen@gmail.com>
2020-11-05 20:05:19 -0600

    ecs v2: entities, components

--- src/ecs.ts
+++ src/ecs.ts
@@ -1,106 +1,220 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { InstanceTypes, Entity } from './instance-types';
-export * from './instance-types';
+import type { Cast, Prepend, Pos, Reverse, Length, Next } from './type-utils';
+export * from './type-utils';
 declare const BitSet: any;
 
-// TODO - should be owned by a "World"
-let componentId = 0;
-const components: any[][] = [];
+type QueryResult<T extends ComponentFactory> =
+  T extends typeof Entity
+    ? number
+    : ReturnType<T>;
 
-export function Component<T extends { new(...args: any): any }>(target: T): T {
-  const currentId = componentId++;
-  target.prototype["@@id"] = currentId;
-  (target as any)["@@id"] = currentId;
-  components[currentId] = [];
-  return target;
+type ReturnTypesInternal<
+  T extends ComponentFactory[],
+  R extends any[] = [],
+  I extends any[] = []
+> = {
+  0: ReturnTypesInternal<
+    T,
+    Prepend<QueryResult<T[Pos<I>]>, R>,
+    Next<I>
+  >
+  1: R
+}[
+  Pos<I> extends Length<T>
+    ? 1
+    : 0
+];
+
+type ReturnTypes<
+  T extends ComponentFactory[]
+> = ReturnTypesInternal<
+  Reverse<T> extends infer U ? Cast<U, any[]> : never,
+  [],
+  []
+>
+
+type Component = Record<string, any>;
+interface ComponentFactory {
+  (...args: any): Component;
 }
 
-// TODO IQueryable fix... to reflect metadata?
-interface IQueryable {
-  "@@id": number;
-}
-interface IQueryableConstructor {
-  new(...args: any) : any;
-}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Queryable {} // Placeholder type to hint to users
-function isQueryable(t: any): t is IQueryable {
-  if (t && typeof t["@@id"] === 'number') {
-    return true;
-  }
-  return false;
+export function Entity (): { "@@entity": true } {
+  throw new Error(`Construct entities with World#entity()`);
 }
 
-let entityId = 0;
-const livingEntities: Map<number, typeof BitSet> = new Map();
-// const dead_entities: Entity[] = [];
+//  World
+//    Resources
+//  ✓ Entities
+//  ✓ Components
+//    Systems
+interface IWorld {
+  register(factory: ComponentFactory): IWorld;
+  get<T extends ComponentFactory>(factory: T, entity: number): ReturnType<T> | null;
+  add<T extends ComponentFactory>(factory: T, entity: number): BoundFactory<void, T>;
+  remove(factory: ComponentFactory, entity: number): void;
+  query<T extends ComponentFactory[]>(...factories: T): QueryBuilder<ReturnTypes<T>[]>;
+  entity(): EntityBuilder;
+  delete(entity: number): void;
+}
 
-export class EntityBuilder {
-  private components: IQueryable[] = [];
+type BoundFactory<R, T extends ComponentFactory> = (...args: Parameters<T>) => R;
 
-  constructor() {
-    this.build.bind(this);
-    this.with.bind(this);
-  }
+interface EntityBuilder {
+  with<T extends ComponentFactory>(factory: T): BoundFactory<EntityBuilder, T>;
+  build(): number;
+}
 
-  build(): number {
-    const currentId = entityId++;
+interface QueryBuilder<R> {
+  not(...factories: ComponentFactory[]): QueryBuilder<R>;
+  result(): R; 
+}
+
+export function World(): IWorld {
+  let componentId = 0;
+  let entityId = 0;
+  const componentsMap: Map<ComponentFactory, Component[]> = new Map();
+  const componentsBit: Map<ComponentFactory, number> = new Map();
+  const entities: Map<number, typeof BitSet> = new Map();
+  const w = window as any;
+  w.componentsMap = componentsMap;
+  w.componentsBit = componentsBit;
+  w.entities = entities;
+
+  function toBitset(factories: ComponentFactory[]): typeof BitSet {
     const bitset = new BitSet();
-    for (let i = 0; i < this.components.length; ++i) {
-      components[this.components[i]["@@id"]][currentId] = this.components[i];
-      bitset.set(this.components[i]["@@id"], 1);
-    }
-    livingEntities.set(currentId, bitset);
-    return currentId;
-  }
 
-  with(component: Queryable): EntityBuilder {
-    if (!isQueryable(component)) throw new TypeError(`Not queryable ${typeof component}`);
-    this.components.push(component);
-    return this;
-  }
-}
-
-export class Query<T extends IQueryableConstructor[]> {
-  private query: IQueryable[];
-  private has = new BitSet();
-  private hasnt = new BitSet();
-
-  constructor(...query: T) {
-    const queryable = Array(query.length);
-    for (const [idx, q] of query.entries()) {
-      if (!isQueryable(q)) throw new TypeError(`Expected Component type, received ${q}`)
-      this.has.set(q["@@id"], 1);
-      queryable[idx] = q;
-    }
-    // safe because it is validated above
-    this.query = queryable;
-  }
-
-  not(...query: IQueryableConstructor[]): Query<T> {
-    for (const q of query) {
-      if (!isQueryable(q)) throw new TypeError(`Expected Component type, received ${q}`)
-      this.hasnt.set(q["@@id"], 1);
-    }
-    return this;
-  }
-
-  // TODO - make to return lazy iterator. -or- should be another function?
-  result(): InstanceTypes<T>[] {
-    const results = [];
-
-    for (const [entity, bitset] of livingEntities.entries()) {
-      if (this.has.and(bitset).equals(this.has) && this.hasnt.and(bitset).equals(0)) {
-        const result = new Array(this.query.length);
-        for (const [idx, query] of this.query.entries()) {
-          result[idx] === Entity
-            ? entity
-            : components[query["@@id"]][entity];
-        }
-        results.push(result);
+    for (const factory of factories) {
+      if (factory === Entity) {
+        continue;
       }
+
+      const bit = componentsBit.get(factory);
+      if (typeof bit === 'undefined') {
+        throw new TypeError(`Attempted to query unknown Component: ${factory.name}`);
+      }
+
+      bitset.set(bit, 1);
     }
 
-    return results as unknown as InstanceTypes<T>[];
+    return bitset;
   }
+
+  let world: IWorld;
+  return world = {
+    register(factory) {
+      componentsMap.set(factory, []);
+      componentsBit.set(factory, componentId++);
+      return world as unknown as IWorld;
+    },
+
+    get<T extends ComponentFactory>(factory: T, entity: number) {
+      const components = componentsMap.get(factory);
+      if (components === undefined) {
+        throw new TypeError(`Attempted to get unknown Component: ${factory.name}`);
+      }
+
+      const component = components[entity];
+      return typeof component === undefined ? null : component as ReturnType<T>;
+    },
+
+    // TODO - lazy iterator?
+    query<T extends ComponentFactory[]>(...factories: T): QueryBuilder<ReturnTypes<T>[]> {
+      const has = toBitset(factories);
+      let hasnt = BitSet();
+
+      let query: QueryBuilder<ReturnTypes<T>[]>;
+      return query = {
+        not(...factories) {
+          hasnt = toBitset(factories);
+          return query;
+        },
+
+        result() {
+          const results: any[] = [];
+          for (const [entity, bitset] of entities.entries()) {
+            if (has.and(bitset).equals(has) && hasnt.and(bitset).equals(0)) {
+              const result = new Array(factories.length);
+              for (const [idx, factory] of factories.entries()) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                result[idx] = factory === Entity ? entity : componentsMap.get(factory)![entity];
+              }
+              results.push(result);
+            }
+          }
+
+          return results as unknown as ReturnTypes<T>[];
+        }
+      };
+    },
+
+    entity() {
+      const entity = entityId++;
+      const parts: [any, any][] = [];
+      let builder: EntityBuilder;
+
+      return builder = {
+        with<T extends ComponentFactory>(factory: T) {
+          return function (...args: Parameters<T>) {
+            parts.push([factory, args]);
+            return builder;
+          }
+        },
+
+        build() {
+          const entityMask = new BitSet();
+          for (const [factory, args] of parts) {
+            const bit = componentsBit.get(factory);
+            const components = componentsMap.get(factory);
+            if (bit === undefined || components === undefined) {
+              throw new TypeError(`Attempted to add unknown Component to entity: ${factory.name}`);
+            }
+
+            components[entity] = factory(args);
+            entityMask.set(bit, 1);
+          }
+          entities.set(entity, entityMask);
+          return entity;
+        }
+      };
+    },
+
+    add(factory, entity) {
+      return (...args) => {
+        const entityMask = entities.get(entity);
+        if (entityMask === undefined) {
+          throw new TypeError(`Attempted to add Component to unknown entity.`);
+        }
+
+        const bit = componentsBit.get(factory);
+        const components = componentsMap.get(factory);
+        if (bit === undefined || components === undefined) {
+          throw new TypeError(`Attempted to add unknown Component to entity: ${factory.name}`);
+        }
+
+        components[entity] = factory(args);
+        entityMask.set(bit, 1);
+      };
+    },
+
+    remove(factory, entity) {
+      const bit = componentsBit.get(factory);
+      const components = componentsMap.get(factory);
+      if (bit === undefined || components === undefined) {
+        throw new TypeError(`Attempted to remove Component from entity that didn't exist: ${factory.name}`);
+      }
+
+      const entityMask = entities.get(entity);
+      if (entityMask === undefined) {
+        throw new TypeError(`Attempted to remove Component from unknown entity.`);
+      }
+
+      delete components[entity];
+      entityMask.set(bit, 0);
+    },
+
+    delete(entity) {
+      // TODO - component clean up... ?
+      entities.delete(entity);
+    },
+  } as IWorld;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,135 +1,104 @@
-import { keyboard } from './keyboard';
-import { EntityBuilder, Query, Entity, Component } from "./ecs";
+import { World, Entity } from './ecs';
 
-const { Application, Sprite, Text, utils } = PIXI; 
-const loader = PIXI.Loader.shared;
-const resources = loader.resources;
+interface IName { name: string }
+const Name = (name: string): IName => ({ name });
 
-let type = "WebGL";
-if (!utils.isWebGLSupported()) {
-  type = "canvas";
+interface IVelocity { vx: number, vy: number }
+const Velocity = (vx: number, vy: number): IVelocity => ({ vx, vy })
+
+const world = World();
+
+world.register(Name);
+world.register(Velocity);
+
+world.entity()
+  .with(Name)("Bob")
+  .build();
+const ray = world.entity()
+  .with(Name)("Ray")
+  .with(Velocity)(-1, -2)
+  .build();
+
+const query = world.query(Entity, Name);
+
+for (const [entity, name] of query.result()) {
+  console.log(`[${entity}]: "${name.name}"`);
 }
 
-utils.sayHello(type);
-
-const app = new Application({
-  width: 512,
-  height: 512,
-  antialias: true,
-  transparent: false,
-  resolution: 1,
-});
-document.body.appendChild(app.view);
-
-loader
-  .add("assets/treasure-hunter.json")
-  .load(setup);
-
-class VelocitySprite extends Sprite {
-  public vx: number;
-  public vy: number;
-  public velocity: {
-    set: (vx: number, vy: number) => void;
-  };
-
-  constructor(texture: PIXI.Texture | undefined) {
-    super(texture);
-    this.vx = 0;
-    this.vy = 0;
-    this.velocity = {
-      set: (vx, vy) => {
-        this.vx = vx; 
-        this.vy = vy;
-      }
-    }
-  }
-
-  tick(delta: number) {
-    this.x += this.vx * delta;
-    this.y += this.vy * delta;
-  }
+for (const [entity, name] of query.not(Velocity).result()) {
+  console.log(`[${entity}]: "${name.name}"`);
 }
 
-function setup() {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const id = resources["assets/treasure-hunter.json"].textures!;
-
-  const dungeon = new Sprite(id["dungeon.png"]);
-  app.stage.addChild(dungeon);
-
-  const explorer = new VelocitySprite(id["explorer.png"]);
-  explorer.x = 68;
-  explorer.y = app.stage.height / 2 - explorer.height / 2;
-  app.stage.addChild(explorer);
-
-  app.ticker.add(delta => gameLoop(delta))
-
-  const treasure = new Sprite(id["treasure.png"]);
-  treasure.x = app.stage.width - treasure.width - 48;
-  treasure.y = app.stage.height / 2 - treasure.height / 2;
-  app.stage.addChild(treasure);
-
-  const door = new Sprite(id["door.png"]);
-  door.x = 32;
-  app.stage.addChild(door);
-
-  const BLOB_COUNT = 6;
-  const BLOB_SPACING = 48
-  const BLOB_OFFSET_X = 150;
-  for (let i = 0; i < BLOB_COUNT; ++i) {
-    const blob = new Sprite(id["blob.png"]);
-    const x = BLOB_SPACING * i + BLOB_OFFSET_X;
-    const y = randomInt(0, app.stage.height - blob.height);
-    blob.position.set(x, y);
-    app.stage.addChild(blob);
-  }
-
-  const left = keyboard('ArrowLeft');
-  const right = keyboard('ArrowRight');
-  const up = keyboard('ArrowUp');
-  const down = keyboard('ArrowDown');
-
-  left.press = () => { explorer.vx -= 2; };
-  left.release = () => { explorer.vx += 2; };
-  up.press = () => { explorer.vy -= 2; }
-  up.release = () => { explorer.vy += 2; }
-  right.press = () => { explorer.vx += 2; };
-  right.release = () => { explorer.vx -= 2; };
-  down.press = () => { explorer.vy += 2; }
-  down.release = () => { explorer.vy -= 2; }
-
-  const style = new PIXI.TextStyle({
-    fill: 'white',
-    stroke: 'black',
-    strokeThickness: 4,
-  });
-  const message = new Text("No collision", style);
-  message.y = 10;
-  message.x = 10;
-  app.stage.addChild(message);
-
-  function gameLoop(delta: number) {
-    explorer.tick(delta);
-
-    if (hitTestRectangle(explorer, treasure)) {
-      message.text = "Hit!";
-      message.tint = 0xff3300;
-    } else {
-      message.text = "No collision..."
-      message.tint = 0xccff99;
-    }
-  }
+world.remove(Velocity, ray);
+for (const [entity, name] of query.not(Velocity).result()) {
+  console.log(`[${entity}]: "${name.name}"`);
 }
 
-function randomInt(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// import { keyboard } from './keyboard';
+// const { Application, Sprite, Text, utils, Container } = PIXI; 
+// const loader = PIXI.Loader.shared;
+// const resources = loader.resources;
 
-function hitTestRectangle(sprite1: PIXI.Sprite, sprite2: PIXI.Sprite): boolean {
-  return !(
-    sprite1.x + sprite1.width < sprite2.x ||
-    sprite1.x > sprite2.x + sprite2.width ||
-    sprite1.y + sprite1.height  < sprite2.y ||
-    sprite1.y > sprite2.y + sprite2.height
-  );
-}
+// let type = "WebGL";
+// if (!utils.isWebGLSupported()) {
+//   type = "canvas";
+// }
+
+// utils.sayHello(type);
+
+// const app = new Application({
+//   width: 512,
+//   height: 512,
+//   antialias: true,
+//   transparent: false,
+//   resolution: 1,
+// });
+// document.body.appendChild(app.view);
+
+// loader
+//   .add("assets/treasure-hunter.json")
+//   .load(setup);
+
+// // TODO - resources
+// let state: (delta: number) => void; // TODO - enum?
+// const gameScene = new Container();
+
+// function setup() {
+//   app.stage.addChild(gameScene);
+
+//   const id = resources["assets/treasure-hunter.json"].textures;
+//   if (typeof id === 'undefined') {
+//     throw new TypeError('Expected resources to load');
+//   }
+
+//   state = play;
+
+//   app.ticker.add((delta: number) => gameLoop(delta));
+// }
+
+// function gameLoop(delta: number) {
+//   // systems
+
+//   // game-state specific:
+//   state(delta);
+// }
+
+// function play(delta: number) {
+// }
+
+// function end() {
+//   // teardown
+// }
+
+// function randomInt(min: number, max: number): number {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function hitTestRectangle(sprite1: PIXI.Sprite, sprite2: PIXI.Sprite): boolean {
+//   return !(
+//     sprite1.x + sprite1.width < sprite2.x ||
+//     sprite1.x > sprite2.x + sprite2.width ||
+//     sprite1.y + sprite1.height  < sprite2.y ||
+//     sprite1.y > sprite2.y + sprite2.height
+//   );
+// }
